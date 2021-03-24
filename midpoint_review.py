3fb6589d92173c9379375985e3d7885459a40ed0
yixi66 <yixiou17@gmail.com>
2021-03-23 20:08:14 -0700

    finish problem 4a

--- midpoint_review.py
+++ midpoint_review.py
@@ -35,3 +35,28 @@ temperature_F = input("What is the current Fahrenheit？")
 temperature_F = int(temperature_F)
 temperature_C = (temperature_F - 32)* 5.0/9.0
 print(str(temperature_C))
+
+
+
+
+# 4a.
+# Hint: You can solve this problem without a forloop, but if you can figure it out using a for loop try to do it that way
+# Write a program that asks the user to input how much money they have saved up. 
+# Then ask the user to input a monthly growth rate (%) example: 5% would mean their savings would increase by 5% monthly. 
+# Calculate how much the user would have at the end of each month for 12 months & print it all to the user
+
+Money = input("How much money did you saved up: ")
+Monthly_growth_rate = input("What is the monthtly growth rate(%): ")
+mouth = 1
+
+Monthly_growth_rate = int(Monthly_growth_rate)
+Monthly_growth_rate = Monthly_growth_rate / 100
+Money = int(Money)
+
+
+while mouth <= 12:
+    Money_after = Money + Money * Monthly_growth_rate
+    print("In", mouth, "mouth, you have ", Money_after)
+
+    mouth = mouth + 1
+    Money = Money_after
