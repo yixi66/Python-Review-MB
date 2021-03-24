d0cb1e4c4049791c1a601b1d8b1eb52edd039ef2
yixi66 <yixiou17@gmail.com>
2021-03-23 18:26:30 -0700

    finished problem 1

---
+++ midpoint_review.py
@@ -0,0 +1,5 @@
+first_name = input("What is your first name: ")
+Last_name = input("What is your last name: ")
+age = input("Enter your age: ")
+
+print("My name is " ,first_name + Last_name, ", I am " ,age ," years old")
